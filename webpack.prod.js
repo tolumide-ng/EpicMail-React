f6b6009d492fb0c5ae47b8ced4fbb97139df548c
tolumide-ng <tolumideshopein@gmail.com>
2019-07-16 07:42:23 +0100

    chore(users): Configure webpack, Install plugins and loaders
    - Adds files to gitignore
    - Adds loaders for styling (sass, css, style)
    - Adss cleanwebpackplugin to remove hash in production
    - Splits webpack for production, development and common configurations
    - Add vendor.js file to src folder to load dependencies
    - Minify css and reuse terserPlugin
    - Install cleanWebpackPlugin to remove previous builds on production
    
    [Maintains #167311760]

---
+++ webpack.prod.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const common = require("./webpack.common");
+const merge = require("webpack-merge");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractWebpackPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = merge(common, {
+  mode: "production",
+  output: {
+    filename: "[name].[contentHash].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new MiniCssExtractWebpackPlugin({ filename: "[name].[contentHash].css" }),
+    new CleanWebpackPlugin(),
+  ],
+  optimization: {
+    minimizer: [
+      new OptimizeCssAssetsPlugin(),
+      new TerserPlugin(),
+      new HtmlWebpackPlugin({
+        template: "./src/template.html",
+        minify: {
+          removeAttributeQuotes: true,
+          collapseWhitespace: true,
+          removeComments: true,
+        },
+      }),
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractWebpackPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+});
